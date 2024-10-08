3aa2b00daad97767bd2e7e320eea18ee495cb46f
alfredo122510 <alfredobladimir01@gmail.com>
2024-10-08 15:07:03 -0500

    Inicializando el repositorio y subiendo la carpeta completa

---
+++ src/main/java/com/sgdupec/upec/rest/Aplicacion.java
@@ -0,0 +1,8 @@
+package com.sgdupec.upec.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+ @ApplicationPath("APIREST")
+public class Aplicacion extends Application{
+   
+}
